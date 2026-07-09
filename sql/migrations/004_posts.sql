396cade36b8791b0166245096c857bb48c2615f9
MhdFiras-3 <MhdFirasAltamimi@protonmail.com>
2026-07-09 21:20:01 +0800

    3: added the post_reads schema, revised the posts schema

--- sql/migrations/004_posts.sql
+++ sql/migrations/004_posts.sql
@@ -1,19 +1,14 @@
 -- +goose Up
 CREATE table posts (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    titel TEXT NOT NULL UNIQUE,
+    title TEXT NOT NULL,
     url TEXT NOT NULL UNIQUE,
     description TEXT,
-    user_id UUID NOT NULL,
     feed_id UUID NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
     published_at TIMESTAMP,
-    last_fetched_at TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (feed_id) REFERENCES feeds(id) ON DELETE CASCADE,
-    UNIQUE (title, feed_id)
-
+    FOREIGN KEY (feed_id) REFERENCES feeds(id) ON DELETE CASCADE
 );
 
 
