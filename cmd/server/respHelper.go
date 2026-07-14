7162f8578121816cecc283ab5998aa421482dfd2
MhdFiras-3 <MhdFirasAltamimi@protonmail.com>
2026-07-14 20:51:37 +0800

    added http response helper functions

---
+++ cmd/server/respHelper.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"log"
+)
+
+func respWithError(w http.ResponseWriter, errMsg string, errStatus int) {
+	type errorMsg struct {
+		Error string `json:"error"`
+	}
+
+	data, err := json.Marshal(errorMsg{Error: errMsg})
+	if err != nil {
+		log.Printf("error encoding data: %v", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(errStatus)
+	_, err = w.Write(data)
+	if err != nil {
+		log.Printf("failed to respond: %v", err)
+	}
+}
+
+func respWithJson(w http.ResponseWriter, respStatus int, payload any) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("error encoding data: %v", err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(respStatus)
+	_, err = w.Write(data)
+	if err != nil {
+		log.Printf("failed to respond: %v", err)
+	}
+}
