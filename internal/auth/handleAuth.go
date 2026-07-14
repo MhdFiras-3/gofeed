2f86388fdddc5744e914dde966c6f8a62f734a2c
MhdFiras-3 <MhdFirasAltamimi@protonmail.com>
2026-07-14 20:07:34 +0800

    added func to create and validate JWT

--- internal/auth/handleAuth.go
+++ internal/auth/handleAuth.go
@@ -4,8 +4,18 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"fmt"
+	"time"
 
 	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type TokenType string
+
+const (
+	TokenTypeAccess  TokenType = "access-token"
+	TokenTypeRefresh TokenType = "refresh-token"
 )
 
 func HashPassword(password string) (string, error) {
@@ -32,4 +42,49 @@ func MakeRefreshToken() (string, error) {
 		return "", fmt.Errorf("failed to generate random byte, %w", err)
 	}
 	return hex.EncodeToString(b), nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    string(TokenTypeAccess),
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+	signedToken, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", fmt.Errorf("error signing token: %w", err)
+	}
+
+	return signedToken, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	parsedToken, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (any, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method")
+		}
+		return []byte(tokenSecret), nil
+	})
+
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("wrong or expired token: %w", err)
+	}
+	idString, err := parsedToken.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("failed to get subject: %w", err)
+	}
+
+	issuer, err := parsedToken.Claims.GetIssuer()
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("failed to get issuer: %w", err)
+	}
+	if issuer != string(TokenTypeAccess) {
+		return uuid.Nil, fmt.Errorf("invalid issuer")
+	}
+	id, err := uuid.Parse(idString)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("failed to parse id: %w", err)
+	}
+	return id, nil
 }
