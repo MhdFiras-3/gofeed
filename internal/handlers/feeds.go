7e97c5e9d365b4cb48a1c85ddc99c52d4521e5b1
MhdFiras-3 <MhdFirasAltamimi@protonmail.com>
2026-07-28 22:12:58 +0800

    Added HandlerCreateFeed

--- internal/handlers/feeds.go
+++ internal/handlers/feeds.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/MhdFiras-3/gofeed/internal/database"
 	"github.com/google/uuid"
@@ -13,6 +15,15 @@ func (cfg *APIConfig) HandlerCreateFeed(w http.ResponseWriter, r *http.Request) 
 	type requestParam struct {
 		URL  string `json:"url"`
 		Name string `json:"name"`
+	}
+	type response struct {
+		ID        uuid.UUID `json:"id"`
+		UserID    uuid.UUID `json:"user_id"`
+		FeedID    uuid.UUID `json:"feed_id"`
+		Name      string    `json:"name"`
+		UserName  string    `json:"user_name"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
 	}
 	var reqData requestParam
 	decoder := json.NewDecoder(r.Body)
@@ -38,14 +49,21 @@ func (cfg *APIConfig) HandlerCreateFeed(w http.ResponseWriter, r *http.Request) 
 		log.Println("failed to get user id from context to create feed")
 		return
 	}
-
-	feedDB, err := cfg.DB.CreateFeed(r.Context(), reqData.URL)
+	tx, err := cfg.DBConn.BeginTx(r.Context(), &sql.TxOptions{})
+	if err != nil {
+		respWithError(w, http.StatusInternalServerError, "something went wrong")
+		log.Printf("failed to begin tx: %v", err)
+		return
+	}
+	defer tx.Rollback()
+	dbTx := cfg.DB.WithTx(tx)
+	feedDB, err := dbTx.CreateFeed(r.Context(), reqData.URL)
 	if err != nil {
 		respWithError(w, http.StatusInternalServerError, "something went wrong")
 		log.Printf("failed to create feed: %v", err)
 		return
 	}
-	_, err = cfg.DB.CreateFeedFollow(r.Context(), database.CreateFeedFollowParams{
+	feedFollowDB, err := dbTx.CreateFeedFollow(r.Context(), database.CreateFeedFollowParams{
 		UserID: userID,
 		FeedID: feedDB.ID,
 		Name:   reqData.Name,
@@ -55,6 +73,18 @@ func (cfg *APIConfig) HandlerCreateFeed(w http.ResponseWriter, r *http.Request) 
 		log.Printf("failed to create feed follow: %v", err)
 		return
 	}
-
-	w.WriteHeader(http.StatusCreated)
+	if err := tx.Commit(); err != nil {
+		respWithError(w, http.StatusInternalServerError, "something went wrong")
+		log.Printf("failed to commit tx: %v", err)
+		return
+	}
+	respWithJson(w, http.StatusCreated, response{
+		ID:        feedFollowDB.ID,
+		UserID:    feedFollowDB.UserID,
+		FeedID:    feedFollowDB.FeedID,
+		Name:      feedFollowDB.Name,
+		UserName:  feedFollowDB.UserName,
+		CreatedAt: feedFollowDB.CreatedAt,
+		UpdatedAt: feedFollowDB.UpdatedAt,
+	})
 }
