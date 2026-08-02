2e0f510bffcbaa1d7dc35bd54060d5186c529cb7
MhdFiras-3 <MhdFirasAltamimi@protonmail.com>
2026-08-02 20:44:47 +0800

    added fetchFeed func

--- internal/scraper/scraper.go
+++ internal/scraper/scraper.go
@@ -1,1 +1,42 @@
 package scraper
+
+import (
+	"context"
+	"encoding/xml"
+	"html"
+	"io"
+	"net/http"
+	"time"
+)
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", "gofeed")
+	client := &http.Client{
+		Timeout: 15 * time.Second,
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	resData, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	rssFeedData := &RSSFeed{}
+	if err := xml.Unmarshal(resData, rssFeedData); err != nil {
+		return nil, err
+	}
+	rssFeedData.Channel.Title = html.UnescapeString(rssFeedData.Channel.Title)
+	rssFeedData.Channel.Description = html.UnescapeString(rssFeedData.Channel.Description)
+	for i := range rssFeedData.Channel.Item {
+		rssFeedData.Channel.Item[i].Title = html.UnescapeString(rssFeedData.Channel.Item[i].Title)
+		rssFeedData.Channel.Item[i].Description = html.UnescapeString(rssFeedData.Channel.Item[i].Description)
+	}
+	return rssFeedData, nil
+}
