fd9d7fb93e68528c11ff02982175bca3387c4bbe
MhdFiras-3 <MhdFirasAltamimi@protonmail.com>
2026-08-03 21:43:00 +0800

    added scrapeFeed func

--- internal/scraper/scraper.go
+++ internal/scraper/scraper.go
@@ -2,11 +2,19 @@ package scraper
 
 import (
 	"context"
+	"database/sql"
 	"encoding/xml"
+	"errors"
 	"html"
 	"io"
+	"log"
 	"net/http"
 	"time"
+
+	"github.com/MhdFiras-3/gofeed/internal/database"
+	"github.com/MhdFiras-3/gofeed/internal/handlers"
+	"github.com/google/uuid"
+	"github.com/lib/pq"
 )
 
 func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
@@ -39,4 +47,53 @@ func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
 		rssFeedData.Channel.Item[i].Description = html.UnescapeString(rssFeedData.Channel.Item[i].Description)
 	}
 	return rssFeedData, nil
+}
+
+func scrapeFeed(ctx context.Context, cfg handlers.APIConfig, feedURL string, feedID uuid.UUID) error {
+	rssDataFeed, err := fetchFeed(ctx, feedURL)
+	if err != nil {
+		return err
+	}
+
+	if err = cfg.DB.MarkFeedFetched(ctx, feedID); err != nil {
+		return err
+	}
+	for _, item := range rssDataFeed.Channel.Item {
+
+		itemNullDescrip := sql.NullString{
+			String: item.Description,
+			Valid:  item.Description != "",
+		}
+		itemNullPub := sql.NullTime{}
+		if item.PubDate != "" {
+			time, err := time.Parse("2006-01-02 15:04:05", item.PubDate)
+			if err != nil {
+				log.Printf("failed to parse time for post url: %s", item.Link)
+			} else {
+				itemNullPub = sql.NullTime{
+					Time:  time,
+					Valid: true,
+				}
+			}
+
+		}
+
+		_, err = cfg.DB.CreatePost(ctx, database.CreatePostParams{
+			Title:       item.Title,
+			Url:         item.Link,
+			Description: itemNullDescrip,
+			FeedID:      feedID,
+			PublishedAt: itemNullPub,
+		})
+		if err != nil {
+			var pqErr *pq.Error
+			if errors.As(err, &pqErr) && pqErr.Code == "23505" {
+				log.Printf("unique violation on constraint: post already exist id: %s", feedID)
+				continue
+			}
+			log.Printf("failed to create post: %v, url: %s", err, item.Link)
+		}
+
+	}
+	return nil
 }
