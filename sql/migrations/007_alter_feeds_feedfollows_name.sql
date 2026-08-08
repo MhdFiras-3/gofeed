c46469102c81ecac8bcc5f75d2645460385aadde
MhdFiras-3 <MhdFirasAltamimi@protonmail.com>
2026-08-08 21:53:16 +0800

    fixed null error on down migration

--- sql/migrations/007_alter_feeds_feedfollows_name.sql
+++ sql/migrations/007_alter_feeds_feedfollows_name.sql
@@ -4,4 +4,6 @@ ALTER TABLE feed_follows ADD COLUMN name TEXT NOT NULL;
 
 -- +goose Down
 ALTER TABLE feed_follows DROP COLUMN name;
-ALTER TABLE feeds ADD COLUMN name TEXT NOT NULL;
+ALTER TABLE feeds ADD COLUMN name TEXT;
+UPDATE feeds SET name = '' WHERE name IS NULL;
+ALTER TABLE feeds ALTER COLUMN name SET NOT NULL;
